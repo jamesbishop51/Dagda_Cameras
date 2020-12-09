0207b645cc4f9c9efeb9734738a429f65f00ce89
james bishop <s00187350@mail.itsligo.ie>
2020-12-09 19:55:34 +0000

    fragment testing finished and working

--- app/src/main/java/edu/jbishop/dagda_cameras/MainActivity.java
+++ app/src/main/java/edu/jbishop/dagda_cameras/MainActivity.java
@@ -1,21 +1,59 @@
 package edu.jbishop.dagda_cameras;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.fragment.app.Fragment;
 import android.os.Bundle;
+import android.view.MenuItem;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import edu.jbishop.dagda_cameras.databinding.ActivityMainBinding;
+import static edu.jbishop.dagda_cameras.R.*;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    ActivityMainBinding binding;
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding=ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        setContentView(layout.activity_main);
 
-        binding.testing.setText("initial commit");
+        BottomNavigationView bottomNav = findViewById(id.bottom_nav);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        if (savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction().replace(id.fragment_container, new Camera_fragment()).commit();
+        }
 
     }
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()) {
+                        case id.nav_Cam:
+                            selectedFragment = new Camera_fragment();
+                            break;
+                        case id.nav_storage:
+                            selectedFragment = new Storage_fragment();
+                            break;
+                        case id.nav_settings:
+                            selectedFragment = new Settings_fragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(id.fragment_container,
+                            selectedFragment).commit();
+                    return true;
+                }
+            };
+
+
 }
+
+
+
