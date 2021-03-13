bf42056ac97af7d48231cd70adab4be6f70c4b99
james bishop <bishbashbosh51@hotmail.com>
2021-03-13 19:35:23 +0000

    added login using auth0 and notifications using firebase and aws sns

--- app/src/main/java/edu/jbishop/dagda_cameras/MainActivity.java
+++ app/src/main/java/edu/jbishop/dagda_cameras/MainActivity.java
@@ -4,9 +4,14 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.iid.FirebaseInstanceId;
+
+import static android.content.ContentValues.TAG;
 import static edu.jbishop.dagda_cameras.R.*;
 
 
@@ -19,7 +24,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(layout.activity_main);
+        setContentView(R.layout.activity_main);
+
+
+        String token = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, token);
+        Toast.makeText(MainActivity.this,token, Toast.LENGTH_SHORT).show();
+
 
         BottomNavigationView bottomNav = findViewById(id.bottom_nav);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
---
+++ app/src/main/java/edu/jbishop/dagda_cameras/services/MyFirebaseInstanceIdService.java
@@ -0,0 +1,20 @@
+package edu.jbishop.dagda_cameras.services;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+import static android.content.ContentValues.TAG;
+
+public class
+MyFirebaseInstanceIdService extends FirebaseInstanceIdService {
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+
+    }
+}
